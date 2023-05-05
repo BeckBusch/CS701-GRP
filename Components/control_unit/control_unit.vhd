cc8d2918c785fc6ffa96716002303c1597943b98
ydan124 <ydan124@aucklanduni.ac.nz>
2023-05-06 11:03:20 +1200

    opcode for instuctions

--- Components/control_unit/control_unit.vhd
+++ Components/control_unit/control_unit.vhd
@@ -2,12 +2,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use work.opcodes.all;
 
 entity Control_Unit is
     Port (
         CLK                 : in  STD_LOGIC;
         Reset               : in  STD_LOGIC;
         Debug_Mode          : in  STD_LOGIC;
+	nios_control        : in  STD_LOGIC;
 	init_up		    : out std_logic; -- uP initialisation
 
 	-- IR
@@ -17,14 +19,16 @@ entity Control_Unit is
        	Rx       	    : in std_logic_vector(3 downto 0);
         Operand             : in std_logic_vector(15 downto 0);
         write_ir            : out std_logic;
+        load_ir	            : out std_logic;
 	ir_reset 	    : in std_logic;
-        ir_load_msb         : in std_logic;
-        ir_load_lsb 	    : in std_logic;
+        ir_load_msb         : out std_logic;
+        ir_load_lsb 	    : out std_logic;
 
         
 	-- Pc:
   	write_pc               : out std_logic;
-	pc_mux_sel 	       : out std_logic_vector(3  downto 0);
+	pc_mux_sel 	       : out std_logic_vector(1  downto 0);
+        reset_pc	       : in std_logic;
 
 	
 	-- RF Block:
@@ -32,6 +36,9 @@ entity Control_Unit is
 	rf_mux_sel 		: out std_logic_vector(2 downto 0);
         rf_mux_sel_x 		: out std_logic;
         rf_mux_sel_z 		: out std_logic;
+        reset_rf : in std_logic;
+        rf_value_sel_x : in std_logic_vector(3 downto 0);
+        rf_value_sel_z : in std_logic_vector(3 downto 0);
 
 	-- memory address and data interface
         write_m_address         : out std_logic;
@@ -40,8 +47,9 @@ entity Control_Unit is
 
 	--AlU
 	alu_mux_A		: out std_logic_vector(1 downto 0);
-	alu_mux_B		: out std_logic_vector(1 downto 0);
-	alu_op			: out std_logic_vector(2 downto 0);
+	alu_mux_B		: out std_logic;
+	alu_op			: out std_logic_vector(1 downto 0)
+
 	
     );
 end Control_Unit;
@@ -72,6 +80,10 @@ begin
      
             when Ini =>
                 -- Add state logic
+
+		
+		
+		
             when Test =>
                 -- Add state logic
             when Test2 =>
@@ -84,11 +96,52 @@ begin
                 -- Add state logic
             when E2 =>
                 -- Add state logic
-            when T0 =>
+            when T0 =>                   --fetch
+
+		next_state <= T1;
+		-- ir <- pm 
+		write_ir <= '1';
+		-- pc <- pc + 1
+		pc_in_sel <= "00";
+		write_pc <= '1';
+
+		
+		-- T1: decoding instruction
+		when T1 =>
+			next_state <= T2;
+			-- detect addressing mode and prepare for execution
+			case opcode(10 downto 9) is
+				when stack => -- stack addressing
+					if opcode(7) = '0' then
+						-- push type operation
+						dm_adr_sel <= selsp2adr;
+						sp_in_sel <= selspop2sp;
+						sp_op_sel <= dec_sp;
+						ld_sp <= '1';
+					else
+						-- pull type operation
+						dm_adr_sel <= selspop2adr;
+						sp_in_sel <= selspop2sp;
+						sp_op_sel <= inc_sp;
+						ld_sp <= '1';
+					end if;
+					-- push: ar <- sp, pull: ar <- sp + 1
+					ld_ar <= '1';
+				when indirect => -- register indirect addressing
+					-- ar <- Ry
+					dm_adr_sel <= selry2adr;
+					ld_ar <= '1';
+				when direct => -- direct addressing
+					-- ar <- ir[24..9]
+					dm_adr_sel <= selir2adr;
+					ld_ar <= '1';
+				when others => -- inherent and immediate
+					-- do nothing
+			end case;
+
+            when T1 =>                           --decode 
                 -- Add state logic
-            when T1 =>
-                -- Add state logic
-            when T2 =>
+            when T2 =>                          --execute
                 -- Add state logic
             when T3 =>
                 -- Add state logic
---
+++ Components/control_unit/opcodes.vhd
@@ -0,0 +1,49 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package opcodes is
+
+-- instruction format
+-- --------------------------------------------------
+-- |AM(2)|OP(6)|ADDR/VAL/OTHERs(16)|Rz(3)|Ry(3)Rx(3)|
+-- --------------------------------------------------
+
+
+-- addressing modes (AM)
+	constant implicit: std_logic_vector(1 downto 0) := "00"; --inherent
+	constant stack: std_logic_vector(1 downto 0) := "01"; --immediate 
+	constant direct: std_logic_vector(1 downto 0) := "10";
+	constant indirect: std_logic_vector(1 downto 0) := "11";
+
+
+-- operations with immediate, direct and indirect AM
+	-- immediate: LDR Rz #value
+	-- direct: LDR Rz $address
+	-- indirect: LDR Rz Ry
+
+	constant ldr	: std_logic_vector(5 downto 0) := "000000";
+	constant str	: std_logic_vector(5 downto 0) := "000010";
+	constant andd	: std_logic_vector(5 downto 0) := "001000";
+	constant  orr	: std_logic_vector(5 downto 0) := "011000";
+	constant add	: std_logic_vector(5 downto 0) := "111000";
+	constant subv	: std_logic_vector(5 downto 0) := "000011";
+	constant sub	: std_logic_vector(5 downto 0) := "000100";
+	constant jmp	: std_logic_vector(5 downto 0) := "011000";
+	constant present	: std_logic_vector(5 downto 0) := "011100";
+	constant dcallbl	: std_logic_vector(5 downto 0) := "101000";
+	constant dcallnb	: std_logic_vector(5 downto 0) := "101001";
+	constant sz		: std_logic_vector(5 downto 0) := "010100";
+	constant clfz	: std_logic_vector(5 downto 0) := "010000";
+	constant cer	: std_logic_vector(5 downto 0) := "111100";
+	constant ceot	: std_logic_vector(5 downto 0) := "111110";
+	constant seot	: std_logic_vector(5 downto 0) := "111111";
+	constant ler	: std_logic_vector(5 downto 0) := "110110";
+	constant ssvop	: std_logic_vector(5 downto 0) := "111011";
+	constant lsip	: std_logic_vector(5 downto 0) := "110111";
+	constant ssop	: std_logic_vector(5 downto 0) := "111010";
+      constant noop	: std_logic_vector(5 downto 0) := "110100";
+	constant max	: std_logic_vector(5 downto 0) := "011110";
+	constant strpc	: std_logic_vector(5 downto 0) := "011101";
+	
+end opcodes;
