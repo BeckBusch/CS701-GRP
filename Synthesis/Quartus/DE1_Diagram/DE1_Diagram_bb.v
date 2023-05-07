4d11256af6631b53512c9da95d48d3f0b37d52e8
fshe388 <fshe388@aucklanduni.ac.nz>
2023-05-07 20:40:55 +1200

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Synthesis/Quartus/DE1_Diagram/DE1_Diagram_bb.v
+++ Synthesis/Quartus/DE1_Diagram/DE1_Diagram_bb.v
@@ -1,22 +1,24 @@
 
 module DE1_Diagram (
 	button_pio_irq_irq,
+	clk_1_clk_clk,
+	clk_1_clk_reset_reset_n,
 	led_pio_external_connection_export,
 	reset_reset_n,
 	sev_seg_pio_external_connection_export,
 	sev_seg_pio_s1_address,
 	sev_seg_pio_s1_readdata,
 	switch_pio_external_connection_export,
-	clk_1_clk_clk,
-	clk_1_clk_reset_reset_n);	
+	clk_clk);	
 
 	output		button_pio_irq_irq;
+	output		clk_1_clk_clk;
+	output		clk_1_clk_reset_reset_n;
 	output	[7:0]	led_pio_external_connection_export;
 	input		reset_reset_n;
 	output	[27:0]	sev_seg_pio_external_connection_export;
 	input	[1:0]	sev_seg_pio_s1_address;
 	output	[31:0]	sev_seg_pio_s1_readdata;
 	input	[7:0]	switch_pio_external_connection_export;
-	output		clk_1_clk_clk;
-	output		clk_1_clk_reset_reset_n;
+	input		clk_clk;
 endmodule
