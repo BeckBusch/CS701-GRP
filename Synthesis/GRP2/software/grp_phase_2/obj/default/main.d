3dcb846d09bae4f8f2a83a01b2bbda774ae49a2c
fshe388 <fshe388@aucklanduni.ac.nz>
2023-06-06 05:00:55 +1200

    compiles and runs on de1-soc properly

---
+++ Synthesis/GRP2/software/grp_phase_2/obj/default/main.d
@@ -0,0 +1,26 @@
+obj/default/main.o: main.c ../grp_phase_2_bsp/system.h \
+ ../grp_phase_2_bsp/linker.h \
+ ../grp_phase_2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../grp_phase_2_bsp//HAL/inc/io.h ../grp_phase_2_bsp//HAL/inc/alt_types.h \
+ ../grp_phase_2_bsp//HAL/inc/alt_types.h \
+ ../grp_phase_2_bsp//HAL/inc/sys/alt_irq.h \
+ ../grp_phase_2_bsp//HAL/inc/nios2.h \
+ ../grp_phase_2_bsp//HAL/inc/priv/alt_legacy_irq.h
+
+../grp_phase_2_bsp/system.h:
+
+../grp_phase_2_bsp/linker.h:
+
+../grp_phase_2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../grp_phase_2_bsp//HAL/inc/io.h:
+
+../grp_phase_2_bsp//HAL/inc/alt_types.h:
+
+../grp_phase_2_bsp//HAL/inc/alt_types.h:
+
+../grp_phase_2_bsp//HAL/inc/sys/alt_irq.h:
+
+../grp_phase_2_bsp//HAL/inc/nios2.h:
+
+../grp_phase_2_bsp//HAL/inc/priv/alt_legacy_irq.h:
